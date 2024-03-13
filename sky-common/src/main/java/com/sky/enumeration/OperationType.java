4d77a3e7dbb60f9a7313e4d2ab278904efaf58f9
cokecola32 <weichixia31@outlook.com>
2024-03-13 16:14:37 +0800

    公共字段自动填充代码实现

--- sky-common/src/main/java/com/sky/enumeration/OperationType.java
+++ sky-common/src/main/java/com/sky/enumeration/OperationType.java
@@ -5,14 +5,14 @@ package com.sky.enumeration;
  */
 public enum OperationType {
 
-    /**
-     * 更新操作
-     */
-    UPDATE,
+        /**
+         * 更新操作
+         */
+        UPDATE,
 
-    /**
-     * 插入操作
-     */
-    INSERT
+        /**
+         * 插入操作
+         */
+        INSERT
 
 }
