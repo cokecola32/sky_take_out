52ee3d6f9f790a6da833b0918127d80563213448
cokecola32 <weichixia31@outlook.com>
2024-03-27 15:14:10 +0800

    用户下单业务代码完成

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -40,4 +41,12 @@ public interface ShoppingCartMapper {
          * @param shoppingCart
          */
         void delete(ShoppingCart shoppingCart);
+
+        /**
+         * 根据用户id删除购物车
+         *
+         * @param userId
+         */
+        @Delete("delete from shopping_cart where user_id = #{userId}")
+        void deleteByUserId(Long userId);
 }
