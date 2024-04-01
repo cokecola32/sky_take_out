1ef28ca7003775894481f291142ca41d309dc61c
cokecola32 <weichixia31@outlook.com>
2024-04-01 15:47:20 +0800

    用户数量统计业务代码完成

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
         /**
@@ -30,4 +32,11 @@ public interface UserMapper {
          */
         @Select("select * from user where id = #{userId}")
         User getById(Long userId);
+
+        /**
+         * 统计用户数量
+         *
+         * @param map
+         */
+        Integer countByMap(Map map);
 }
